8f93eaa8d5e89388eb634f75067216ccca76b329
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-07 09:04:32 -0700

    Add properties to each model.

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,10 +1,20 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 using System;
 
 namespace Factory.Models
 {
   public class Engineer
   {
-
+    public Engineer()
+    {
+      this.Machines = new HashSet<EngineerMachine>();
+    }
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+    
+    [DisplayName("Hire Date")]
+    public DateTime HireDate { get; set; }
+    public virtual ICollection<EngineerMachine> Machines { get; set; }
   }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,10 +1,21 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 using System;
 
 namespace Factory.Models
 {
   public class Machine
   {
-
+    public Machine()
+    {
+      this.Engineers = new HashSet<EngineerMachine>();
+    }
+    public int MachineId { get; set; }
+    public string Type { get; set; }
+    public string Description { get; set; }
+    
+    [DisplayName("Date of Installation")]
+    public DateTime Installation { get; set; }
+    public virtual ICollection<EngineerMachine> Engineers { get; set; }
   }
 }
