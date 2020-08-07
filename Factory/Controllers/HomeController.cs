db5096a587fed76f2766b2b1c5c46b5e33c0be1b
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-08-07 16:47:26 -0700

    Add database access to HomeController.

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,20 @@
-using Factory.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
 using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
